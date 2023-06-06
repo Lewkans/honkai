cae1e6f20ebbe5ef7e17c8d30e9f26c98cc1a7fb
Lewkans <toh.lewis@gmail.com>
2023-06-06 12:20:04 +1000

    First push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+// You have to import image to use it
+import { BrowserRouter as Router, Routes, Route, Link, useNavigate } from 'react-router-dom';
+import Honkai from './components/Honkai';
 
-function App() {
+import { Box, Button, Container, Tab, Tabs, Typography } from '@mui/material';
+
+const App = () => {
+  const navigate = useNavigate();
+  const [width, setWidth] = React.useState(window.innerWidth);
+  const [tab, setTab] = React.useState(0)
+  const resize = () => {
+    setWidth(window.innerWidth);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={tab} onChange={(e, n) => {setTab(n)}}>
+          <Tab label={width <= 800 ? 'H' : 'Home'} onClick={() => navigate("/")}/>
+          <Tab label={width <= 800 ? 'H' : 'Honkai'} onClick={() => navigate("/honkai")}/>
+        </Tabs>
+      </Box>
+      <Container>
+        <Routes>
+          <Route
+            path="/honkai"
+            element={
+              <Honkai />
+            }
+          />
+          <Route
+            path="/"
+            element={
+              <Box sx={{ width: '100%'}}>
+                <Typography sx={{ color: 'red' }}>
+                  Please choose an option from the nav bar
+                </Typography>
+              </Box>
+            }
+          />
+        </Routes>
+      </Container>
+    </Box>
   );
 }
 
---
+++ src/components/Honkai.js
@@ -0,0 +1,102 @@
+import React from 'react';
+import {data} from '../data/honkaidata.js';
+import { TextField, Typography } from '@mui/material';
+
+const Honkai = () => {
+    const MAX_CAPACITY = 180
+    const FUEL = 60
+    const IMMERSIFIER = 40
+    const RATIO = 5
+    
+    const [currLevel, setCurrLevel] = React.useState(1);
+    const [currExp, setCurrExp] = React.useState(0);
+    const [fuel, setFuel] = React.useState(0);
+    const [immersifier, setImmersifier] = React.useState(0);
+    const [goalLevel, setGoalLevel] = React.useState(2);
+        
+    const getEquilibriumLevel = (level) => {
+        if (level >= 65) {
+            return 6;
+        }
+        if (level < 20) {
+            return 0;
+        }
+        return Math.floor(level/10) - 1;
+    }
+    
+    const isValidLevel = (level) => {
+        return level > 0 &&
+            level <= data.exp.length && 
+            Number.isInteger(parseInt(level));
+    }
+    
+    const totalExp = (level) => {
+        if (!isValidLevel(level)) {
+            return 0;
+        }
+        return data.exp.slice(0, parseInt(level)).reduce((prev, curr) => prev + curr, 0);
+    }
+      
+    return (
+    <>
+        <Typography>Current Level: (Max {data.exp.length - 1})</Typography>
+        <TextField
+            inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+            onChange={(e) => {
+                (e.target.value === "" || /^[0-9\b]+$/.test(e.target.value)) && setCurrLevel(e.target.value)
+            }}
+            value={currLevel}
+            error={!isValidLevel(currLevel)}
+            placeholder='Enter your current level'
+        />
+        <Typography>Equilibrium Level: {getEquilibriumLevel(currLevel)}</Typography>
+        <Typography>Current xp:</Typography>
+        <TextField
+            inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+            onChange={(e) => {
+                (e.target.value === "" || /^[0-9\b]+$/.test(e.target.value)) && setCurrExp(e.target.value)
+            }}
+            value={currExp}
+            error={!(currExp >= 0 && currExp < data.exp[parseInt(currLevel)])}
+            placeholder='Enter your current exp'
+        />/{data.exp[parseInt(currLevel)]}
+        <Typography>Goal Level: (Max {data.exp.length})</Typography>
+        <TextField
+            inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+            onChange={(e) => {
+                (e.target.value === "" || /^[0-9\b]+$/.test(e.target.value)) && setGoalLevel(e.target.value)
+            }}
+            value={goalLevel}
+            error={!isValidLevel(goalLevel) || parseInt(currLevel) >= parseInt(goalLevel)}
+            placeholder='Enter your goal level'
+        />
+        <Typography>Fuel:</Typography>
+        <TextField
+            inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+            onChange={(e) => {
+                (e.target.value === "" || /^[0-9\b]+$/.test(e.target.value)) && setFuel(e.target.value)
+            }}
+            value={fuel}
+        />
+        <Typography>Immersifier:</Typography>
+        <TextField
+            inputProps={{ inputMode: 'numeric', pattern: '[0-9]*' }}
+            onChange={(e) => {
+                (e.target.value === "" || /^[0-9\b]+$/.test(e.target.value)) && setImmersifier(e.target.value)
+            }}
+            value={immersifier}
+        />
+        <Typography>Total exp: {totalExp(goalLevel)}</Typography>
+        <Typography>Diff exp: {totalExp(goalLevel) - totalExp(currLevel) - currExp}</Typography>
+        <Typography>Daily exp gain: {24 * RATIO * 10 + 5 * data.daily[getEquilibriumLevel(currLevel)]}</Typography>
+        <Typography>Fuel exp: {FUEL * fuel * RATIO}</Typography>
+        <Typography>Immersifier exp: {IMMERSIFIER * immersifier * RATIO}</Typography>
+        <Typography>Time to reach level: ~{
+            (totalExp(goalLevel) - totalExp(currLevel) - currExp - FUEL * fuel * RATIO - IMMERSIFIER * immersifier * RATIO)/
+            (24 * RATIO * 10 + 5 * data.daily[getEquilibriumLevel(currLevel)])
+        } days</Typography>
+        <Typography variant='h6'>Note: Takes full day (24hrs) into account when calculating daily exp gain</Typography>
+    </>)
+}
+
+export default Honkai;
